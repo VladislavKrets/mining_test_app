77c03eadaafde1f0759f8c1e1518440df58e6daa
lollipop <vladislav.krets@gmail.com>
2020-04-13 21:56:29 +0300

    added total_cost endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path
+
 from api import views
 from rest_framework.routers import SimpleRouter
 
@@ -5,3 +7,6 @@ router = SimpleRouter()
 router.register(r'resources', views.ResourceViewSet, basename='resources')
 
 urlpatterns = router.urls
+urlpatterns += [
+    path('total_cost/', views.TotalCostApiView.as_view())
+]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,21 @@
-from rest_framework import viewsets
+from django.db.models import Sum
+from rest_framework import viewsets, status
+from rest_framework import views
+from rest_framework.response import Response
 from api import serializers
 from api import models
+from django.db.models import F, FloatField
 
-
+#for all resoures endpoints
 class ResourceViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ResourceModelSerializer
     queryset = models.ResourceModel.objects.all()
+
+#for total_cost endpoint
+class TotalCostApiView(views.APIView):
+    def get(self, request):
+        total_cost = models.ResourceModel.objects.all().aggregate(
+                total=Sum(F('price') * F('amount'), output_field=FloatField()))['total'] or 0
+        return Response(data={'total_cost': total_cost},
+                        status=status.HTTP_200_OK)
+
